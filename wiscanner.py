94995af25e6c098777827c85e72c0b6bc3feabb7
unknown <Henning.Stoerk@WM-WZRH-HES.corp.unic.com>
2014-07-16 09:15:08 +0200

    Minor improvements

--- wiscanner.py
+++ wiscanner.py
@@ -11,6 +11,16 @@ from twython import Twython
 from pushover import Pushover
 
 # ==========================================================
+try:
+	nm = nmap.PortScanner()
+except nmap.PortScannerError:
+	print('Nmap not found', sys.exc_info()[0])
+	sys.exit(0)
+except:
+	print("Unexpected error:", sys.exc_info()[0])
+	sys.exit(0)
+
+# ==========================================================
 class Device:
 	allDevices = set()
 	devicesInHouse = set()
@@ -22,6 +32,7 @@ class Device:
 		self.macAddress = macAddress
 		self.name = name
 		self.thresholdSeconds = thresholdSeconds
+		self.visible = False
 		Device.allDevices.add(self)
 			  
 	# ======================================================
@@ -31,11 +42,12 @@ class Device:
 		timePrefix = '{0}: '.format(self.lastJoinTime.strftime("%d.%m.%Y %H:%M"))
 		message = '{0} entered {1}.'.format(self.name, LOCATION)
 		if len(Device.devicesInHouse) > 0:
-			message += ' Already there: {0}.'.format(Device.joinDeviceNames(Device.devicesInHouse))
+			message += ' Already here: {0}.'.format(Device.joinDeviceNames(Device.devicesInHouse))
 		else:
-			message += ' He is alone.'
+			message += ' No one else is currently here.'.format(self.name)
 		Device.__sendMessage(timePrefix + message)
 		Device.devicesInHouse.add(self)
+		self.writeStatusFile()
 
 	# ======================================================
 	def __reportLeave(self):
@@ -46,19 +58,42 @@ class Device:
 		message = '{0} left {1} '.format(self.name, LOCATION)
 		message += '(last seen {0}).'.format(self.lastVisibleTime.strftime("%d.%m.%Y %H:%M"))
 		if len(Device.devicesInHouse) > 0:
-			message += ' Still there: {0}'.format(Device.joinDeviceNames(Device.devicesInHouse))
+			message += ' Still here: {0}.'.format(Device.joinDeviceNames(Device.devicesInHouse))
 		else:
-			message += ' He was alone.'
+			message += ' No one else was here.'.format(self.name)
 		Device.__sendMessage(timePrefix + message)
+		self.writeStatusFile()
 
 	# ======================================================
-	def __reportInvisible(self):
+	def writeStatusFile(self):
+		statusDir = 'status/'
+		outFile = statusDir + self.name + '.out'
+		inFile = statusDir + self.name + '.in'
+
+		if (self.inHouse):
+			deleteFile = outFile
+			touchFile = inFile
+		else:
+			deleteFile = inFile
+			touchFile = outFile			
+
+		open(touchFile, 'a').close()
+		try:
+			os.remove(deleteFile)
+		except OSError:
+			pass
+
+	# ======================================================
+	def reportInvisible(self):
+		self.visible = False
+
 		# If device is invisible, is regarded to be here currently and was not visible during the threshold amount of seconds, it probably left. Oh noez!
 		if (self.inHouse == True and self.secondsSinceLastVisible() > self.thresholdSeconds):
 			self.__reportLeave()
 
 	# ======================================================
-	def __reportVisible(self):
+	def reportVisible(self):
+		self.visible = True
 		self.lastVisibleTime = datetime.now()
 		
 		# If device is visible but is not known to be here currently, then it just joined. Woohoo!
@@ -85,38 +120,25 @@ class Device:
 		return s
 
 	# ======================================================
-	def scanNetwork(self, ipRange):
-		try:
-			nm = nmap.PortScanner()
-		except nmap.PortScannerError:
-			print('Nmap not found', sys.exc_info()[0])
-			sys.exit(0)
-		except:
-			print("Unexpected error:", sys.exc_info()[0])
-			sys.exit(0)
-
-		nm.scan(hosts= ipRange + '/24', arguments='-n -sP -PE -T5')
-		hosts_list = [(nm[x]['addresses']) for x in nm.all_hosts()]
-		for host in hosts_list:
-			if 'mac' in host and host['mac'] == self.macAddress:
-				self.__reportVisible()
-				return
-		self.__reportInvisible()
-
-	# ======================================================
 	@staticmethod
 	def __sendToTwitter(text):
-		twitter = Twython(TWITTER_APP_KEY, TWITTER_APP_SECRET, TWITTER_OAUTH_TOKEN, TWITTER_OAUTH_TOKEN_SECRET)
-		twitter.update_status(status=text)
+		try:
+			twitter = Twython(TWITTER_APP_KEY, TWITTER_APP_SECRET, TWITTER_OAUTH_TOKEN, TWITTER_OAUTH_TOKEN_SECRET)
+			twitter.update_status(status=text)
+		except:
+			print('Twitter exception!')
 
 	# ======================================================
 	@staticmethod
 	def __sendToPushover(text):
-		po = Pushover(PUSHOVER_TOKEN)
-		po.user(PUSHOVER_USER)
-		msg = po.msg(text)
-		msg.set("title", PUSHOVER_TITLE)
-		po.send(msg) 
+		try:
+			po = Pushover(PUSHOVER_TOKEN)
+			po.user(PUSHOVER_USER)
+			msg = po.msg(text)
+			msg.set("title", PUSHOVER_TITLE)
+			po.send(msg)
+		except:
+			print('Pushover exception!')
 
 	# ======================================================
 	@staticmethod
@@ -137,13 +159,21 @@ class Device:
 		return ", ".join(deviceNamesList)
 
 	# ======================================================
+	@staticmethod
+	def joinStrings(deviceList):
+		devicesList = [(str(x)) for x in deviceList]
+		return ", ".join(devicesList)
+
+	# ======================================================
 	def __str__(self):
-		return '{0} | {1} | {2} | {3} | {4} | {5}'.format(self.name, self.macAddress, self.inHouse, self.lastVisibleTime, self.lastJoinTime, self.lastLeaveTime)
+		visible = '+' if self.visible else '-'
+		inHouse = 'i' if self.inHouse else 'o'
+		return '{0}({1}|{2})'.format(self.name, visible, inHouse)
 
 	# ==========================================================
 	def pickle(self):
 		try:
-			device_file = open(self.name + '.pkl', 'wb')
+			device_file = open('pickle/' + self.name + '.pkl', 'wb')
 			pickle.dump(self, device_file)
 			device_file.close()
 		except:
@@ -152,7 +182,7 @@ class Device:
 	# ==========================================================
 	def unpickle(self):
 		try:
-			device_file = open(self.name + '.pkl', 'rb')
+			device_file = open('pickle/' + self.name + '.pkl', 'rb')
 			tempDevice = pickle.load(device_file)
 			self.inHouse = tempDevice.inHouse
 			self.lastVisibleTime = tempDevice.lastVisibleTime
@@ -166,16 +196,15 @@ class Device:
 
 # === End Class Device =====================================
 
-
-
 # ==========================================================
 if __name__ == '__main__':
 	config = configparser.ConfigParser()
-	config.read('config.ini')
+	config.read('config/config.ini')
 
 	LOCATION = config['General']['Location']
 	IP_RANGE = config['General']['IpRange']
 	INTERVAL = int(config['General']['ScanInterval'])
+	DEBUG = config['General'].getboolean('DebugMode')
 
 	TWITTER_APP_KEY = config['Twitter']['AppKey']
 	TWITTER_APP_SECRET = config['Twitter']['AppSecret']
@@ -187,20 +216,33 @@ if __name__ == '__main__':
 	PUSHOVER_TITLE = config['Pushover']['Title']
 
 	for section in config.sections():
-		if (section[:7] == 'Device_'):	
-			print(section[7:])
+		if (section[:7] == 'Device_'):
 			newDevice = Device(config[section]['MacAddress'], section[7:], int(config[section]['Threshold']))
 	
 	while True:
+		nm.scan(hosts= IP_RANGE + '/24', arguments='-n -sP -PE -T5')
+		hostsList = [(nm[x]['addresses']) for x in nm.all_hosts()]
+
 		for device in Device.allDevices:
 			# unpickle
 			device.unpickle()
 
 			# visible in network?
-			device.scanNetwork(IP_RANGE)
+			visible = False
+			for host in hostsList:
+				if 'mac' in host and host['mac'] == device.macAddress:
+					visible = True
+					continue
+			
+			if visible:
+				device.reportVisible()
+			else:
+				device.reportInvisible()
 
 			# pickle
 			device.pickle()
 
-		#print('all devices: {0} | in house: {1}'.format(Device.joinDeviceNames(Device.allDevices), Device.joinDeviceNames(Device.devicesInHouse)))
+		if DEBUG:
+			print('{0}: {1}'.format(datetime.now(), Device.joinStrings(Device.allDevices)))
+		
 		time.sleep(INTERVAL)
